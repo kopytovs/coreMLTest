cb4142a495d016c9c409434407b96989b3a3e7ca
kopytovs <kopytov@me.com>
2018-02-06 19:14:36 +0300

    Initial Commit

---
+++ testCoreMLObjFind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testCoreMLObjFind
+//
+//  Created by Sergey Kopytov on 06.02.2018.
+//  Copyright © 2018 Sergey Kopytov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
